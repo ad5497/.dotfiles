326d17fcdbddb611d1b3cc4e56914079215434b8
alex dong <dongalex0602@gmail.com>
2024-06-30 05:12:05 -0400

    change keymaps and default filesystem view for neo-tree.lua

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,26 +1,29 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim", 
-  branch = "v3.x", 
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = {
-    "nvim-lua/plenary.nvim", 
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim", 
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window
-  }, 
+  },
   config = function()
-    local config = require("neo-tree").setup({
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = "hide",
+        },
+      },
       event_handlers = {
         {
-          event = "file_opened", 
-          handler = function(file_path)
-            -- auto close
-            -- vimc.cmd("Neotree close")
-            -- OR
+          event = "file_opened",
+          handler = function()
             require("neo-tree.command").execute({ action = "close" })
           end
         }
-      }
+      },
     })
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+    vim.keymap.set('n', '\\', ':Neotree toggle<cr>', { desc = 'NeoTree toggle' })
+    vim.keymap.set('n', '|', ':Neotree toggle current reveal_force_cwd<cr>', { desc = 'NeoTree toggle fill window and cwd' })
+  end,
 }
