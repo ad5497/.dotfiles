a4a0fdb0cceeac9dc8266236776b51205252c632
alex dong <dongalex0602@gmail.com>
2024-06-30 05:10:56 -0400

    add some new keymaps to lsp-config.lua

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,31 +1,38 @@
 return {
   {
-    "williamboman/mason.nvim", 
+    "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
-  }, 
+    end,
+  },
   {
-    "williamboman/mason-lspconfig.nvim", 
+    "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { 
-          "lua_ls", 
-          "basedpyright"
-        }
+        ensure_installed = {
+          "lua_ls",
+          "basedpyright",
+        },
       })
-    end
-  }, 
+    end,
+  },
   {
-    "neovim/nvim-lspconfig", 
+    "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.basedpyright.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "[G]oto [D]efinition" })
+      vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, { desc = "[G]oto [R]eferences" })
+      vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "[G]oto [I]mplementation" })
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type [D]efinition" })
+      vim.keymap.set("n", "<leader>ds", require('telescope.builtin').lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
+      vim.keymap.set("n", "<leader>ws", require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = "[W]orkspace [S]ymbols" })
+    end,
+  },
 }
