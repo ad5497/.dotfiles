72b4fcf5f36b7e683cb172bed6648d12c80cbd7a
alex dong <dongalex0602@gmail.com>
2024-06-29 23:16:18 -0400

    fix auto_install in config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,20 +5,6 @@ return {
     local config = require("nvim-treesitter.configs")
     config.setup({
       auto_install = true, 
-      ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "java",
-        "javascript",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "vim",
-        "vimdoc"
-      },
       highlight = { enable = true }, 
       indent = { enable = true }, 
     }) 
