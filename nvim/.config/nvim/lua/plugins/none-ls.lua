75868bf890271a5b6608674a1e97904b95120f30
alex dong <dongalex0602@gmail.com>
2024-06-30 05:13:15 -0400

    change keymap for fixing format in none-ls.lua

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,16 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua, -- lua formatter
-        null_ls.builtins.formatting.black, -- python formatter
-        null_ls.builtins.formatting.isort, -- python formatter (for imports i think)
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua, -- lua formatter
+				null_ls.builtins.formatting.black, -- python formatter
+				null_ls.builtins.formatting.isort, -- python formatter (for imports i think)
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { desc = "[F]ix [F]ormat" })
+	end,
 }
